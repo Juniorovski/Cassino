6580f774e7a38c39d54b11a3a63ec14f2a2b768e
Junior <juniornine19@gmail.com>
2025-04-02 01:13:27 -0300

    cassino Online

---
+++ scripts/index.js
@@ -0,0 +1,81 @@
+const btn_Giro = document.getElementById("btn-girar");
+
+
+function giro() {
+    
+    const frutas = ["maca", "pera", "moeda"];
+    const imagens = {
+        maca: "public/images/maca-br.png",
+        pera: "public/images/pera.png",
+        moeda: "public/images/moeda.png",
+    };
+
+    // Gera três elementos aleatórios da lista
+    const element1 = frutas[Math.floor(Math.random() * frutas.length)];
+    const element2 = frutas[Math.floor(Math.random() * frutas.length)];
+    const element3 = frutas[Math.floor(Math.random() * frutas.length)];
+
+    console.log(`${element1} ${element2} ${element3}`);
+
+    // Atualiza as imagens na tela
+    const display = document.querySelector(".display");
+    
+    display.innerHTML = `
+        <img id="triangulo" src="public/images/triangulo.png" alt="triangulo">
+        <img src="${imagens[element1]}" alt="${element1}">
+        <img src="${imagens[element2]}" alt="${element2}">
+        <img src="${imagens[element3]}" alt="${element3}">
+        <img id="trianguloB" src="public/images/triangulo.png" alt="triangulo">
+    `;
+
+    // Verifica se os três elementos são iguais
+    if (element1 === element2 && element2 === element3) {
+        
+        const amount = document.getElementById("amount");
+        const valor = parseFloat(amount.innerText.replace("R$", ""));
+        if(element1 === "moeda" && element2 === "moeda" && element3 === "moeda"){
+            novoSaldo = valor + 30; // Adiciona R$30 ao saldo
+        }
+        if(element1 === "maca" && element2 === "maca" && element3 === "maca"){
+            novoSaldo = valor + 10; // Adiciona R$10 ao saldo
+        }
+        if(element1 === "pera" && element2 === "pera" && element3 === "pera"){
+            novoSaldo = valor + 10; // Adiciona R$10 ao saldo
+        }
+        amount.innerText = `R$${novoSaldo.toFixed(2)}`; // Atualiza o saldo na tela
+        console.log("Você ganhou!" + novoSaldo);
+
+    } else {
+        console.log("Você perdeu! Tente novamente.");
+
+        const amount = document.getElementById("amount");
+        const valor = parseFloat(amount.innerText.replace("R$", ""));       
+        const novoSaldo = valor - 5; // Subtrai R$5 do saldo
+        amount.innerText = `R$${novoSaldo.toFixed(2)}`; // Atualiza o saldo na tela
+        if (novoSaldo < 0) {
+            console.log("Saldo insuficiente! Adicione mais saldo a carteira e continue jogando.");
+            amount.innerText = `R$0.00`; // Reseta o saldo para 0
+            return;
+        }
+ 
+       }  
+ }
+
+btn_Giro.addEventListener("click", giro);
+
+document.getElementById("add-saldo").addEventListener("click",()=>{
+ 
+    let input = document.getElementById("input-saldo").value;
+    const amount = document.getElementById("amount");
+    const valor = parseFloat(amount.innerText.replace("R$",""));
+    input = valor + parseFloat(input); // Adiciona o valor do input ao saldo atual
+    if (input < 0) {
+        console.log("Valor inválido! O saldo não pode ser negativo.");
+        return;
+    }
+    amount.innerText = `R$${input.toFixed(2)}`; 
+   
+});
+
+
+
